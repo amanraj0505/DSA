ecdd4a4fa55b68e6d23b8cf5303ccc2956bdc4d4
Aman Raj <aman.raj@unisys.com>
2022-10-28 18:17:30 +0530

    pattern2

---
+++ pattern2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int count = 1;
+    int count1 = 0;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1;i<=n;++i){
+        count = 1;
+        for(int j = n-i+1;j>=1;--j){
+            cout<<count++;
+        }
+        for(int k = 1;k<=count1*2;++k){
+            cout<<'*';
+        }
+        for(int m = n-i+1;m>=1;--m){
+            cout<<m;
+        }
+        count1++;
+        cout<<endl;
+    }
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+count = n;
+        // for(int m = n-i+1;m>=1;m--){
+        //     cout<<count--;
+        // }
